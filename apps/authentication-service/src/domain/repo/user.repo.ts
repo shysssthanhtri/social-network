209e4e850c636eb43260b895b572704a6e979213
shysssthanhtri <shyssthanhtri@gmail.com>
2025-03-03 22:50:25 +0700

    feat: check email duplicated

--- apps/authentication-service/src/domain/repo/user.repo.ts
+++ apps/authentication-service/src/domain/repo/user.repo.ts
@@ -1,6 +1,10 @@
 import { UserEntity } from '@/domain/entities/user.entity';
 
 export abstract class UserRepo {
+    abstract findByEmail(
+        email: UserEntity['email'],
+    ): Promise<UserEntity | null>;
+
     abstract add(
         user: Pick<UserEntity, 'email' | 'hashedPassword' | 'passwordVersion'>,
     ): Promise<UserEntity>;
--- apps/authentication-service/src/domain/use-cases/sign-up/sign-up.use-case.ts
+++ apps/authentication-service/src/domain/use-cases/sign-up/sign-up.use-case.ts
@@ -1,4 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 import { UserRepo } from '@/domain/repo/user.repo';
 import { SignUpReqDto } from '@/domain/use-cases/sign-up/dtos/sign-up.req.dto';
@@ -9,20 +11,39 @@ export class SignUpUseCase {
     constructor(
         @Inject(UserRepo)
         private readonly userRepo: UserRepo,
+        @InjectConnection()
+        private readonly connection: Connection,
     ) {}
 
     async execute(dto: SignUpReqDto): Promise<SignUpResDto> {
-        const hashedPassword = 'faked';
-        const passwordVersion = 1;
-        const user = await this.userRepo.add({
-            email: dto.email,
-            hashedPassword,
-            passwordVersion,
-        });
-        console.log(user);
-        return {
-            accessToken: '',
-            refreshToken: '',
-        };
+        const session = await this.connection.startSession();
+        session.startTransaction();
+
+        try {
+            const isEmailDuplicated = !!(await this.userRepo.findByEmail(
+                dto.email,
+            ));
+            if (isEmailDuplicated) {
+                throw new ConflictException();
+            }
+
+            const hashedPassword = 'faked';
+            const passwordVersion = 1;
+            const user = await this.userRepo.add({
+                email: dto.email,
+                hashedPassword,
+                passwordVersion,
+            });
+            console.log(user);
+
+            await session.commitTransaction();
+            return {
+                accessToken: '',
+                refreshToken: '',
+            };
+        } catch (err) {
+            await session.abortTransaction();
+            throw err;
+        }
     }
 }
