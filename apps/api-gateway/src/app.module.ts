5afc0c5a0a6b28bfeda8061cc2724ff80fdd1eff
shysssthanhtri <shyssthanhtri@gmail.com>
2025-03-10 07:39:11 +0700

    feat: add log to api-gateway

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        LoggerModule.forRoot({
+            pinoHttp: {
+                name: 'add some name to every JSON line',
+                level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+                transport:
+                    process.env.NODE_ENV !== 'production'
+                        ? { target: 'pino-pretty' }
+                        : undefined,
+            },
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,10 +1,21 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+    const logger = app.get(Logger);
+    app.useLogger(logger);
+    app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+    const configService = app.get(ConfigService);
+    const port = configService.getOrThrow<string>('API_GATEWAY_PORT');
+    await app.listen(port, () => {
+        logger.log(`Running on port ${port}...`);
+    });
 }
 bootstrap().catch((err) => {
     console.error(err);
