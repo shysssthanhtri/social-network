5eeac7843105add2887e68fa5f96722bb93d6dcd
shysssthanhtri <shyssthanhtri@gmail.com>
2025-03-15 17:16:37 +0700

    feat: init posts-service typeorm

--- apps/posts-service/src/app.module.ts
+++ apps/posts-service/src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from 'common-nestjs-server';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                url: configService.getOrThrow<string>('POST_DATABASE_URL'),
+                entities: [],
+                synchronize: true,
+                logging: true,
+            }),
+            inject: [ConfigService],
+        }),
+        TypeOrmModule.forFeature([]),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- apps/posts-service/src/main.ts
+++ apps/posts-service/src/main.ts
@@ -1,9 +1,14 @@
 import { CommonNestApp } from 'common-nestjs-server';
+import { TransactionInterceptor } from 'nestjs-postgresql';
+import { DataSource } from 'typeorm';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await CommonNestApp.bootstrap(AppModule);
+
+    const dataSource = app.getApp().get(DataSource);
+    app.getApp().useGlobalInterceptors(new TransactionInterceptor(dataSource));
 
     await app.listen('POSTS_PORT');
 }
